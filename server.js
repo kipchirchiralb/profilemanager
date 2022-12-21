fdbc4a5c9415583189dbc077a79903ab73907ac0
Kipchirchir Albert <albertkipchirchir02@gmail.com>
2022-12-21 14:18:19 +0300

    profiles

--- server.js
+++ server.js
@@ -25,10 +25,23 @@ app.set("view engine", "ejs")
 app.get('/', (req, res) => {
     res.render('index')
 })
+app.get('/profiles', (req, res) => {
+    db.query(`SELECT * FROM profiles`, (err, result)=>{
+        res.render('profiles', {profiles: result})                
+    })
+})
+
 app.post("/new-profile", upload.single("profileimage"), (req, res) => {
     console.log(req.body)
     console.log(req.file)
-    res.render('profiles')
+    let sql = `INSERT INTO profiles(fullName, email,imageOriginalName, imageName, bio) VALUES('${req.body.fullname}','${req.body.email}','${req.file.originalname}','${req.file.filename}','${req.body.bio}')`
+    db.query(sql, (err)=>{
+        if(err){
+            res.redirect('/')
+        }else{
+            res.redirect('/profiles')
+        }
+    })
 })
 
 app.listen(3000, ()=>console.log("server listening to port 3000"))
